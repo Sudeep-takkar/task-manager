eca406ccb39998c7e2f039fb782bb71e816b61c8
sudeep-takkar <sudeep.takkar@yahoo.com>
2024-03-07 19:01:23 -0500

    react routing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
-// import DefaultComponentWithNameChanged from './Clock';
-// import { add } from './Clock';
-
-import Tasks  from './components/FunctionalComponentExample'
-import Form from './components/Form';
-import StatefulComponent from './components/StatefulComponent';
-
-const taskList = [
-  {
-  task: "Task 1",
-  isComplete: false
-  },
-  {
-    task: "Task 2",
-    isComplete: false
-  },
-  {
-    task: "Task 3",
-    isComplete: false
-  },
-  {
-    task: "Task 4",
-    isComplete: true
-  },
-  {
-    task: "Task 5",
-    isComplete: true
-  }
-]
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 function App() {
-  // console.log(add)
   return (
-    <div className="App">
-      {/* <DefaultComponentWithNameChanged /> */}
-      {/* <Tasks taskList={taskList} someRandomProp={'1536'} /> */}
-      {/* <Form /> */}
-      <StatefulComponent />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
     </div>
   );
 }
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
 
 export default App;
