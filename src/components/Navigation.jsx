81ca843ce852e02f7e9c037b2ee781b9444de658
sudeep-takkar <sudeep.takkar@yahoo.com>
2024-03-12 19:09:04 -0400

    Feat react form

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Navigation = () => {
+  return (
+    <div className="border-b bg-slate-100">
+      <nav className="container flex justify-between items-center">
+        <div className="font-semibold">React Forms</div>
+      </nav>
+    </div>
+  )
+}
